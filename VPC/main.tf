6cf299f8020a520ed97e808092a41ee0c363ca43
Vosit Islomov <102islomov@gmail.com>
2022-03-30 20:51:57 -0400

    Updated main and variables files

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,6 +1,6 @@
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
     Name = "main"
   }
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 # Create Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.pub_subnet_cidr
   availability_zone = "us-east-1a"
   tags = {
     Name = "Public Subnet"
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet" {
 # Create Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.prv_subnet_cidr
   availability_zone = "us-east-1b"
   tags = {
     Name = "Private Subnet"
