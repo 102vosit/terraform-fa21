cf35979572d2d111672e74c64dff42f240ebdf89
Vosit Islomov <102islomov@gmail.com>
2022-05-11 19:41:04 -0400

    Customized to use as a child module

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,14 +1,13 @@
 resource "aws_instance" "dev" {
   ami           = "ami-0c02fb55956c7d316"
+  subnet_id     = var.subnet_id
   instance_type = "t2.micro"
   tags = {
     "Name" = "web-server"
   }
 }
 
-resource "aws_vpc" "ec2_vpc" {
-  cidr_block = "30.10.0.0/16"
-  tags = {
-    "Name" = "ec2-vpc"
-  }
+variable "subnet_id" {
+  type    = string
+  default = ""
 }
