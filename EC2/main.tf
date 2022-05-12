7ed1223ea8f75b8be7110aac53353eff560d907e
Vosit Islomov <102islomov@gmail.com>
2022-05-11 21:30:44 -0400

    EC2 Folder changed

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,13 +1,19 @@
 resource "aws_instance" "dev" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = var.ami_id
   subnet_id     = var.subnet_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     "Name" = "web-server"
   }
 }
 
 variable "subnet_id" {
-  type    = string
-  default = ""
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+variable "instance_type" {
+  type = string
 }
