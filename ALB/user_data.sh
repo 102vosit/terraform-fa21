575db04d90bb6a3e18a1b79339d42acb0ecb53b2
Vosit Islomov <102islomov@gmail.com>
2022-06-29 22:55:00 -0400

    UserData changed project based app

--- ALB/user_data.sh
+++ ALB/user_data.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-sudo su
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+#!/bin/bash -ex
+yum -y install httpd php mysql php-mysql
+chkconfig httpd on
+service httpd start
+if [ ! -f /var/www/html/lab-app.tgz ]; then
+cd /var/www/html
+wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/CUR-TF-200-ACACAD/studentdownload/lab-app.tgz
+tar xvfz lab-app.tgz
+chown apache:root /var/www/html/rds.conf.php
+fi
